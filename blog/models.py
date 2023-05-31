1236ce371a26e34d1afc737a9cf6978e3d04112d
unuefeejovwoke <vuwill7114@gmail.com>
2023-05-30 23:17:24 -0700

    setup views, urls and template for post list and details

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+    
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -17,6 +23,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
