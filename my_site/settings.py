b6c6d1e1cd52c9073dd1c2cda7eba5429266c61b
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-03 21:49:00 -0700

    send mail trial

--- my_site/settings.py
+++ my_site/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'seu7.tech@gmail.com'
+EMAIL_HOST_PASSWORD = 'fhruxkbrxejxjasw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
