a8153b6be45dd4dfe9fd394b62d64503b8996cb6
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-08 20:46:31 -0700

    post share to email

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 class PostListView(ListView): 
@@ -39,14 +40,22 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id): 
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
 
     if request.method == "POST": 
         form = EmailPostForm(request.POST)
 
         if form.is_valid(): 
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = cd['name'] + " recommends you read " + post.title
+            message = "Read " + post.title + " at " + post_url + "\n\n" + cd['name'] + "'s comments: " + cd['comments']
+            send_mail(subject, message, 'seu7.tech@gmail.com', [cd['to']])
+
+            sent = True
+
 
     else: 
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'post': post,'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,'form': form, 'sent': sent,})
