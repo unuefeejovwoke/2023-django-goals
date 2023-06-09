cf2cb45cd8d6f0f6774fd8880344b53266415f73
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-08 22:27:06 -0700

    comment func

--- blog/models.py
+++ blog/models.py
@@ -50,3 +50,20 @@ class Post(models.Model):
                                         self.slug])
 
     
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+    related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ['created']
+        indexes = [
+        models.Index(fields=['created']),
+        ]
+    def __str__(self):
+        return 'Comment by ' + self.name + ' on ' + str(self.post)
